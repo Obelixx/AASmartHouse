31d124f015c33ef534b5fd3967f2862fe52947fe
Obelixx <Obelixx@users.noreply.github.com>
2016-04-08 23:03:56 +0300

    Moved Data and Models to new projects

---
+++ AAWebSmartHouse/Data/AAWebSmartHouse.Data/AAWebSmartHouseDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace AAWebSmartHouse.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+
+    public class AAWebSmartHouseDbContext : IdentityDbContext<User>
+    {
+        public AAWebSmartHouseDbContext()
+                : base("DefaultConnection", throwIfV1Schema: false)
+            {
+        }
+
+        public static AAWebSmartHouseDbContext Create()
+        {
+            return new AAWebSmartHouseDbContext();
+        }
+    }
+}
