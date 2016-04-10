cc74d2e59adedd3fe969d80ae37c6fe70a992bf7
Obelixx <Obelixx@users.noreply.github.com>
2016-04-10 19:25:36 +0300

    added MySQL

--- AAWebSmartHouse/Data/AAWebSmartHouse.Data/AAWebSmartHouseDbContext.cs
+++ AAWebSmartHouse/Data/AAWebSmartHouse.Data/AAWebSmartHouseDbContext.cs
@@ -6,7 +6,7 @@
     public class AAWebSmartHouseDbContext : IdentityDbContext<User>
     {
         public AAWebSmartHouseDbContext()
-                : base("DefaultConnection", throwIfV1Schema: false)
+                : base("LocalMySQLConnection", throwIfV1Schema: false)
             {
         }
 
